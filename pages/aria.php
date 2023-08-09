d6bf064d635738206eb211771d2c185223eea979
obama-max <chancier88@gmail.com>
2023-08-09 15:14:11 +0300

    Updated content and added examples

--- pages/aria.php
+++ pages/aria.php
@@ -2,7 +2,7 @@
 		ARIA (Accessible Rich Internet Applications):
 	</h2>
 	<p class="pt-4 fs-6">	
-		<a href="https://www.w3.org/WAI/standards-guidelines/aria/">ARIA</a> е набор от атрибути, които могат да бъдат добавени към <a href="https://developer.mozilla.org/en-US/docs/Web/HTML">HTML</a> елементи, за да подобрят достъпността на динамичното и интерактивно съдържание. Той помага на помощните технологии да разберат целта и поведението на различните елементи.
+		<a href="https://www.w3.org/WAI/standards-guidelines/aria/">ARIA</a> е набор от атрибути, които могат да бъдат добавени към <a href="https://developer.mozilla.org/en-US/docs/Web/HTML">HTML</a> елементи, за да подобрят достъпността на динамичното и интерактивно съдържание. Той помага на помощните технологии да разберат целта и поведението на различните елементи. Чете се главно от помощните технологии за да можем да доставим по-добре и по-лесно преживяване на потребителите с недостатъци.
 	</p>   
 	
 	<p class="pt-4 fs-4">	
@@ -39,6 +39,11 @@
 		</div>
 	</div>
 	<p class="pt-4 fs-6">	
-		Употребата на <a href="https://www.w3.org/WAI/standards-guidelines/aria/">ARIA</a> атрибути и роли е иновативен и важен начин за подобряване на достъпността на уеб сайтовете и приложенията. При разработка и проектиране на уеб сайтове, разработчиците трябва да имат предвид възможностите на <a href="https://www.w3.org/WAI/standards-guidelines/aria/">ARIA</a> и да го използват, където е необходимо, за да предоставят по-добра достъпност и удобство на потребителите с увреждания.
+		Употребата на <a href="https://www.w3.org/WAI/standards-guidelines/aria/">ARIA</a> атрибути и роли е иновативен и важен начин за подобряване на достъпността на уеб сайтовете и приложенията. При разработка и проектиране на уеб сайтове, разработчиците трябва да имат предвид възможностите на <a href="https://www.w3.org/WAI/standards-guidelines/aria/">ARIA</a> и да го използват, където е необходимо, за да предоставят по-добра достъпност и удобство на потребителите с увреждания. Има поговорка че "Никоя <a href="https://www.w3.org/WAI/standards-guidelines/aria/">ARIA</a> не е по-добра от лошата <a href="https://www.w3.org/WAI/standards-guidelines/aria/">ARIA</a>." причината за това е че след голям анализ над милиони страници беше намерено че повече от 41% от тях имаха грешки. Трябва да се обърне сериозно внимание или ще се направят повече проблеми отколкото удобства.
 	</p>
-
+	<p class="codepen" data-height="300" data-theme-id="dark" data-default-tab="html,result" data-slug-hash="NWEVrOw" data-editable="true" data-user="obama-max" style="height: 300px; box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid; margin: 1em 0; padding: 1em;">
+  <span>See the Pen <a href="https://codepen.io/obama-max/pen/NWEVrOw">
+  Untitled</a> by Darius Popov (<a href="https://codepen.io/obama-max">@obama-max</a>)
+  on <a href="https://codepen.io">CodePen</a>.</span>
+</p>
+	
