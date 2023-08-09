2a1dfea07602638daf410f9def5cf3ecd3121e23
BarinHD <bobimetodiev159@gmail.com>
2023-08-09 14:59:35 +0300

    Removed unnecessary head elemets

--- builders/head.php
+++ builders/head.php
@@ -5,11 +5,7 @@
 <meta name="author" content="Дарчо Сладкиша и Боян сладоледа">
 <meta name="generator" content="Hugo 0.115.4">
 
-
-<link rel="stylesheet" href="assets/slick/slick.css">
 <link rel="stylesheet" href="assets/style/sidepagestyle2.css">
 <link rel="stylesheet" href="assets/bootstrap/bootstrap.css">
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Lora:wght@400;500;600;700&family=Ysabeau+Office:wght@1;100;200;300;400;500;600700;800;900;1000&display=swap" rel="stylesheet">
-
