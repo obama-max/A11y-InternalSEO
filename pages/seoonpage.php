6b8747889f5512d05772b712ad55c13f813f37dc
obama-max <chancier88@gmail.com>
2023-08-09 15:15:49 +0300

    Added a new page

---
+++ pages/seoonpage.php
@@ -0,0 +1,68 @@
+
+<div class="col-md-9 border border-2 fs-4 flex-column border-dark d-flex rounded-4 me-2 pb-5 " tabindex="0">
+	<h2 class="pt-4 fs-4">
+		SEO-onpage
+	</h2>
+
+	<p class="pt-4 fs-6">
+    SEO-onpage представлява важна част от оптимизацията на уебсайта ни с цел подобрение на видимостта му в търсачки като Google и Microsoft Bing. 
+    Този процес се фокусира върху вътрешни аспекти на сайта ни, които влият на начина, по който търсачките го разбират и оценяват.
+
+    Когато хората търсят продукти, услуги или информация, свързана с нашата сфера, SEO-onpage ни помага да се появим по-високо в резултатите на търсенето. 
+    Това включва оптимизация на дизайна на сайта, текстовете, метаданните и алтернативния текст на снимките.
+
+
+	</p>
+
+	<p class="pt-4 fs-4">	
+		Ето някои ключови аспекти за SEO-onpage:
+	</p>	
+
+	<div class="row fs-6">
+		<div class="col-6">
+			<p>
+				<ul>
+					<li>
+						Хубав дизайн и удобен сайт
+					</li>	
+					<li class="mt-2">
+						Добра структура на HTML 
+					</li>	
+					<li class="mt-2"> 
+						Метаданни като заглавие и описание
+					</li>
+                    <li class="mt-2"> 
+						Алтернативни текстовете за снимки и видео елемнти
+					</li>                  
+				</ul>	 
+			</p>     
+		</div>
+
+		<div class="col-6">
+			<p>
+				<ul>
+					<li>
+						Да имаме хубаво ползено и оптимизирано съдържание
+					</li>	
+					<li class="mt-2">
+						Добра скорост на уебсайта
+					</li>
+                    <li class="mt-2"> 
+						Линкове към нашият сайт които помагат с навигирането
+					</li> 
+				</ul>	
+			</p>     
+		</div>
+	</div>	
+	
+	<p class="pt-4 fs-6">
+    SEO оптимизацията като процес не е лесен, нито пък става бързо. Отнема време, усилия, ресурси и много проучване за да имаме уебсайт които наистина да може да се качи най-отгоре на търсачките. Трябва не само да направим добър дизайн и бърз удобен начин да се минава през сайта, но и също така да се оптимизира и поддържа в бъдеще.
+	</p>
+	<p class="codepen" data-height="300" data-theme-id="dark" data-default-tab="html,result" data-slug-hash="wvQbWNr" data-editable="true" data-user="obama-max" style="height: 300px; box-sizing: border-box; display: flex; align-items: center; justify-content: center; border: 2px solid; margin: 1em 0; padding: 1em;">
+  <span>See the Pen <a href="https://codepen.io/obama-max/pen/wvQbWNr">
+  Subtitles example</a> by Darius Popov (<a href="https://codepen.io/obama-max">@obama-max</a>)
+  on <a href="https://codepen.io">CodePen</a>.</span>
+</p>
+<script async src="https://cpwebassets.codepen.io/assets/embed/ei.js"></script>
+</div>
+
