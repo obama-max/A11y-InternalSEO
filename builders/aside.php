d94cef56af3aed93044af8eec7bba6e7b8c56090
BarinHD <bobimetodiev159@gmail.com>
2023-08-10 14:49:18 +0300

    Added logic, automation + my head hurts

--- builders/aside.php
+++ builders/aside.php
@@ -1,270 +1,34 @@
 <div class="container__aside">
-
-<p class="pt-4 ">	
-	<ul class="fs-6">
-		<li>
-			<a href="?page=wcag">
-				Ръководствата за достъпност на уеб съдържание (WCAG):
-			</a>
-		</li>
-		<li>
-			<a href="?page=percievable">
-				Възприемливост:
-			</a>
-		</li>
-		<li>
-			<a href="?page=operable">
-				Изпълнимост:
-			</a>
-		</li>
-		<li>
-			<a href="?page=understandable">
-				Разбираемост:
-			</a>
-		</li>
-		<li>
-			<a href="?page=robust">
-				Надеждност:
-			</a>
-		</li>
-        <li>
-			<a href="?page=aria">
-				ARIA:
-			</a>
-		</li>
-		<li>
-			<a href="?page=semantic">
-				Семантичен HTML:
-			</a>
-		</li>
-		<li>
-			<a href="?page=keyboard">
-				Достъпност с клавиатура:
-			</a>
-		</li>
-		<li>
-			<a href="?page=captions">
-				Субтитри и транскрипции:
-			</a>
-		</li>
-		<li>
-			<a href="?page=focus">
-				Управление на фокуса:
-			</a>
-		</li>
-        
-		<li>
-			<a href="?page=testing">
-				Тестване и проверка:
-			</a>
-		</li>
-	</ul>	
-</p> 
-                  
-<div class="sidebar close"> 
+  <div class="sidebar "> 
     <ul class="nav-links">
-      <li>
-        <a href="#">
-          <i class='bx bx-grid-alt' ></i>
-          <span class="link_name">Dashboard</span>
-        </a>
-        <ul class="sub-menu blank">
-          <li><a class="link_name" href="#">Category</a></li>
-        </ul>
-      </li>
-      <li>
-        <div class="iocn-link">
-          <a href="#">
-            <i class='bx bx-collection' ></i>
-
-            <span class="link_name">Category</span>
-          </a>
-          <i class='bx bxs-chevron-down arrow' ></i>
-        </div>
-
-        <ul class="sub-menu">
-          <li>
-            <a class="link_name" href="#">
-              Category
-            </a>
-          </li>
-
-          <li>
-
+    <?php foreach ( $pages as $page_link => $page ) { ?>
+      <?php
+        $sub_pages = isset($page['sub_pages']) && is_array($page['sub_pages']) ? $page['sub_pages'] : [];
+      ?>
+        <li>
+          <div class="icon-link">
             <a href="#">
-              HTML & CSS
+              <i class='bx bx-collection' ></i>
+
+              <a class="link_name" href='?page=<?php echo $page_link ?>'>
+                <?php echo $page['page_data'] ?>
+              </a>
             </a>
-          </li>
-
-          <li>
-            <a href="#">
-              JavaScript
-            </a>
-          </li>
-
-          <li>
-            <a href="#">
-              PHP & MySQL
-            </a>
-          </li>
-        </ul>
-      </li>
-
-      <li>
-        <div class="iocn-link">
-          <a href="#">
-            <i class='bx bx-book-alt' ></i>
-
-            <span class="link_name">Posts</span>
-          </a>
-
-          <i class='bx bxs-chevron-down arrow' ></i>
-        </div>
-
-        <ul class="sub-menu">
-          <li>
-            <a class="link_name" href="#">
-              Posts
-            </a>
-          </li>
-
-          <li>
-            <a href="#">
-              Web Design
-            </a>
-          </li>
-
-          <li>
-            <a href="#">
-              Login Form
-            </a>
-          </li>
-
-          <li>
-            <a href="#">
-              Card Design
-            </a>
-          </li>
-        </ul>
-      </li>
-
-      <li>
-        <a href="#">
-          <i class='bx bx-pie-chart-alt-2' ></i>
-
-          <span class="link_name">Analytics</span>
-        </a>
-
-        <ul class="sub-menu blank">
-          <li>
-            <a class="link_name" href="#">
-              Analytics
-            </a>
-          </li>
-        </ul>
-      </li>
-
-      <li>
-        <a href="#">
-          <i class='bx bx-line-chart' ></i>
-
-          <span class="link_name">Chart</span>
-        </a>
-
-        <ul class="sub-menu blank">
-          <li>
-            <a class="link_name" href="#">
-              Chart
-            </a>
-          </li>
-        </ul>
-      </li>
-      
-      <li>
-        <div class="iocn-link">
-          <a href="#">
-            <i class='bx bx-plug' ></i>
-
-            <span class="link_name">Plugins</span>
-          </a>
-
-          <i class='bx bxs-chevron-down arrow' ></i>
-        </div>
-
-        <ul class="sub-menu">
-          <li>
-            <a class="link_name" href="#">
-              Plugins
-            </a>
-          </li>
-
-          <li>
-            <a href="#">
-              UI Face
-            </a>
-          </li>
-
-          <li>
-            <a href="#">
-              Pigments
-            </a>
-          </li>
-
-          <li>
-            <a href="#">
-              Box Icons
-            </a>
-          </li>
-        </ul>
-      </li>
-
-      <li>
-        <a href="#">
-          <i class='bx bx-compass' ></i>
-
-          <span class="link_name">Explore</span>
-        </a>
-
-        <ul class="sub-menu blank">
-          <li>
-            <a class="link_name" href="#">
-              Explore
-            </a>
-          </li>
-        </ul>
-      </li>
-
-      <li>
-        <a href="#">
-          <i class='bx bx-history'></i>
-
-          <span class="link_name">History</span>
-        </a>
-        
-        <ul class="sub-menu blank">
-          <li>
-            <a class="link_name" href="#">
-              History
-            </a>
-          </li>
-        </ul>
-      </li>
-
-      <li>
-        <a href="#">
-          <i class='bx bx-cog' ></i>
-
-          <span class="link_name">Setting</span>
-        </a>
-
-        <ul class="sub-menu blank">
-          <li>
-            <a class="link_name" href="#">
-              Setting
-            </a>
-          </li>
-        </ul>
-      </li>
+            <i class='bx bxs-chevron-down arrow' ></i>
+          </div>
+          <?php if ( count($sub_pages) ) { ?>
+          <ul class="sub-menu">
+            <?php foreach ( $sub_pages as $sub_page_link => $sub_page ) { ?>
+            <li>
+              <a href='?page=<?php echo $sub_page_link ?>'>
+                <?php echo $sub_page['page_data'] ?>
+              </a>
+            </li>
+            <?php } ?>
+          </ul>
+          <?php } ?>
+        </li>
+        <?php } ?>
     </ul>
   </div>
 </div>
