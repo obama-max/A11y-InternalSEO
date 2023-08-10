0a84e7199133dbbcb9da599f7c61aab8db0937b9
obama-max <chancier88@gmail.com>
2023-08-10 14:56:10 +0300

    Ne sum siguren kakvo she stane

--- builders/maincontent.php
+++ builders/maincontent.php
@@ -5,10 +5,12 @@
 
 			</p>
             <div class="hack">p</div>
-            <div class="col-md-12 fs-4 flex-column d-flex me-2 p-5 container__body-div " tabindex="0">
-            <h2 class="pt-4 fs-4">
-		Субтитри и транскрипции:
-	</h2>
+
+            <div class="col-md-12 fs-4 flex-column d-flex rounded-4 me-2 p-5 " tabindex="0">
+                     
+				<?php 
+                    include $page_path;
+                ?>
 
     		</div>
 		</div>
